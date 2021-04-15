90d2bbe43794891067e8aa8bcc6d15ce46bf61f0
Joe Han <qiyu_han@brown.edu>
2021-04-15 00:05:13 -0400

    vis script

--- chess.rkt
+++ chess.rkt
@@ -8,16 +8,37 @@ option max_tracelength 10 -- NEEDED! default is 5. need to be able to find the w
 **/
 
 -- We define our board with rows and columns that have previous and next neighbors.
-sig row {r_prev: lone row, r_next: lone row}
-sig col {c_prev: lone col, c_next: lone col}
+abstract sig row {r_prev: lone row, r_next: lone row}
+abstract sig col {c_prev: lone col, c_next: lone col}
+
+one sig colA extends col {}
+one sig colB extends col {}
+one sig colC extends col {}
+one sig colD extends col {}
+one sig colE extends col {}
+one sig colF extends col {}
+one sig colG extends col {}
+one sig colH extends col {}
+
+one sig row1 extends row {}
+one sig row2 extends row {}
+one sig row3 extends row {}
+one sig row4 extends row {}
+one sig row5 extends row {}
+one sig row6 extends row {}
+one sig row7 extends row {}
+one sig row8 extends row {}
 
 -- We have squares that can optionally match to a piece occupying it.
-sig square {var pc: lone piece}
+sig square {
+  var pc: lone piece,
+  coord: set row -> col
+  }
 
 -- A piece optionally matches to a square that it occupies, plus any squares it can move to. 
 abstract sig piece {
   var sq: lone square,
-  var moves: set square
+  var moves: set square -- TODO how to make this single?
 }
 
 -- These represent our chess pieces, with the letter corresponding to the piece name in algebraic notation (excepting pawns). 
@@ -58,10 +79,53 @@ pred structural { -- solely focused on board dimensions
   all c: col | {c.(*c_next + *c_prev) = col and (*c_next  + *c_prev).c = col} 
   -- bidirectional - this.next's prev = this
   all c1: col | all c2: col | {c1.c_next = c2 implies c2.c_prev = c1}
+
+  -- enforces 8x8 coordinates.
+  colA.c_next = colB
+  colB.c_next = colC
+  colC.c_next = colD
+  colD.c_next = colE
+  colE.c_next = colF
+  colF.c_next = colG
+  colG.c_next = colH
+
+  row1.r_next = row2
+  row2.r_next = row3
+  row3.r_next = row4
+  row4.r_next = row5
+  row5.r_next = row6
+  row6.r_next = row7
+  row7.r_next = row8
+
+  -- maps each square to its coords
+  all sq: square | all r: row | all c: col | {
+      r->c->sq in Board.places implies { r->c = sq.coord } 
+    }
+}
+
+pred piecesToSquares {
+  -- bidirectional mapping
+  all p: piece | all s: square {
+    s in p.sq implies p in s.pc
+  }
+
+  -- unique
+  all p: piece | {
+    lone pc.p
+  }
+}
+
+pred validKings {
+  #(K) = 2
+  all k: K | {
+    some k.sq
+    some pc.k
+  }
 }
 
 pred validBoard { -- position legality 
-
+  validKings
+  piecesToSquares
 }
 
 pred KMoves[k: King]{
@@ -79,10 +143,24 @@ pred KMoves[k: King]{
 -- function that returns a diagonal
 -- hard code diagonals into squares
 
-pred moveK
+pred moveK {
+
+}
+
+pred init {
+  all p: piece | {
+    some p.sq
+  }
+}
 
 pred generatePuzzle {
   structural
+  init
+  always { validBoard }
 }
 
-run {generatePuzzle} for exactly 8 col, exactly 8 row, exactly 64 square, exactly 1 piece, exactly 1 K
+fun topDiag[sq: square]: lone square {
+  { square } -- TODO
+}
+
+run {generatePuzzle and validBoard} for exactly 8 col, exactly 8 row, exactly 64 square, exactly 5 piece, exactly 2 K, exactly 1 B, exactly 1 Q, exactly 1 N
--- script.js
+++ script.js
@@ -4,115 +4,51 @@ function printValue(row, col, xoffset, yoffset, value) {
   d3.select(svg)
     .append("text")
     .style("fill", "black")
-    .attr("x", row*25 + xoffset)
-    .attr("y", col*36 + yoffset)
+    .attr("x", row*75 + xoffset)
+    .attr("y", col*75 + yoffset)
     .attr("font-size", "xx-large")
     .text(value);
 }
 
-function convert(value) {
-  const str = value.toString().substring(0, 1);
-  if(str == ''){
+function convert(square) {
+  const squareContent = square.pc.toString()
+  console.log(squareContent)
+  if(squareContent == '') {
     return '_'
   } else {
-    return str
+    return squareContent
   }
 }
 
-function printBoard(boardAtom, xoffset, yoffset) {
-  let colSort = orderCols()
-  let rowSort = orderRows()
-  for (r = 0; r < rowSort.length; r++) {
-    for (c = 0; c < colSort.length; c++) {
-      console.log([rowSort[r]].toString() + ", " + [colSort[c]].toString() + " = " + convert(boardAtom.places[rowSort[r]][colSort[c]]))
-      printValue(r, c, xoffset, yoffset,
-                 convert(boardAtom.places[rowSort[r]][colSort[c]]))  
-    }
-  }   
+function toCoord(rc) {
+  return rc.charAt(3);
 }
 
-function orderRows(){
-  let ra = []
-  let rmap = new Map()
-  let rns = row_neighbors.tuples()
-  for(n = 0; n < rns.length; n++){
-    tup = rns[n].atoms()
-    curr = rmap.get(tup[0])
-    if(curr != undefined){
-      curr.push(tup[1])
-      rmap.set(tup[0], curr)
-    } else {
-      rmap.set(tup[0], [tup[1]])
+// printValue(row1, colA, 5, 25, Board0.places[row1][colA])
+
+function printBoard(startRow, startCol, board) {
+  let row = startRow;
+  let col = startCol;
+
+  
+  for (r = 0; r < 8; r++) {
+    printValue(0, r, 50, 50, toCoord(row.toString()));
+    for (c = 1; c < 9; c++) {
+      const square = Board0.places[row][col];
+      printValue(c, r, 50, 50, convert(square));
+//       printValue(c, r, 50, 50, square.coord.toString())
+//       console.log(convert(square));
+      col = col.c_next;
     }
-  }
-  let first_entry;
-  for(let [key, value] of rmap.entries()){
-    if(value.length == 2){
-      first_entry = [key, value]
-    } else if(value.length == 1){
-      ra.push(key)
-      return ra
-    }
+    col = startCol;
+    row = row.r_prev;
   }
   
-  let r_prev = first_entry[0]
-  let r_curr = first_entry[1].find(e => e != r_prev)
-  ra.push(r_prev)
-  ra.push(r_curr)
-  while(true){
-    let crt = rmap.get(r_curr)
-    if(crt.length == 2){
-      break
-    } else {
-      let newr = crt.find(e => e != r_prev && e != r_curr)
-      ra.push(newr)
-      r_prev = r_curr
-      r_curr = newr
-    }
-  }
-  return ra
-}
-
-function orderCols() {
-  let ca = []
-  let cmap = new Map()
-  let cns = col_neighbors.tuples()
-  for(m = 0; m < cns.length; m++){
-      tup = cns[m].atoms()
-      curr = cmap.get(tup[0])
-      if(curr != undefined){
-        curr.push(tup[1])
-        cmap.set(tup[0], curr)
-      } else {
-        cmap.set(tup[0], [tup[1]])
-      }
-    }
-  let first_entry;
-  for(let [key, value] of cmap.entries()){
-    if(value.length == 2){
-      first_entry = [key, value]
-    } else if(value.length == 1){
-      ca.push(key)
-      return ca
-    }
+  for (c = 1; c < 9; c++){
+    printValue(c, 8, 50, 50, toCoord(col.toString()));
+    col = col.c_next;
   }
   
-  let c_prev = first_entry[0]
-  let c_curr = first_entry[1].find(e => e != c_prev)
-  ca.push(c_prev)
-  ca.push(c_curr)
-  while(true){
-    let cct = cmap.get(c_curr)
-    if(cct.length == 2){
-      break
-    } else {
-      let newc = cct.find(e => e != c_prev && e != c_curr)
-      ca.push(newc)
-      c_prev = c_curr
-      c_curr = newc
-    }
-  }
-  return ca
 }
 
-printBoard(Board0, 5, 25)
+printBoard(row8, colA, Board0);
