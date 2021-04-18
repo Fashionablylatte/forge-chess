98099bb9863221c3706da69f78a50a8d33ab520d
Harshini <57264480+harshini225@users.noreply.github.com>
2021-04-17 23:13:23 -0700

    images js

--- script.js
+++ script.js
@@ -1,66 +1,118 @@
 d3.selectAll("svg > *").remove();
 
-function make_piece(url) {
-  const img = document.createElement("img")
-  img.src = url
-  img.style.width = '100%'
-  img.style.height = '15%'
-  img.style.display = 'block'
-  img.style['margin-bottom'] = '10%'
-  
-  return img;
-}
+const BOARD_SIZE = 5;
+const OFFSET = 50;
 
 function printValue(row, col, xoffset, yoffset, value) {
     switch(value) {
       case "BN0":
+      case "BN1":
         var myimage = d3.select(svg).append('image')
-          .attr('xlink:href', 'http://clipart-library.com/images/8TGKgnpTa.png')
-          .attr("x", row*75 + xoffset*1)
-          .attr("y", col*75 + yoffset*1)
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_010.png')
+          .attr("x", row*75 + xoffset)
+          .attr("y", col*75 + yoffset)
           .attr('width', 50)
           .attr('height', 50);
          break;
       case "WN0":
+      case "WN1":
         var myimage = d3.select(svg).append('image')
-          .attr('xlink:href', 'http://clipart-library.com/images/pio5AeRaT.jpg')
-          .attr("x", row*75 + xoffset*1)
-          .attr("y", col*75 + yoffset*1)
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_004.png')
+          .attr("x", row*75 + xoffset)
+          .attr("y", col*75 + yoffset)
           .attr('width', 50)
           .attr('height', 50);
          break;
       case "WR0":
+      case "WR1":
         var myimage = d3.select(svg).append('image')
-          .attr('xlink:href', 'http://clipart-library.com/images/8cxXrazcp.jpg')
-          .attr("x", row*60 + xoffset*1)
-          .attr("y", col*60 + yoffset*1)
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_005.png')
+          .attr("x", row*70 + xoffset)
+          .attr("y", col*65 + yoffset)
+          .attr('width', 50)
+          .attr('height', 50);
+         break;
+      case "BR0":
+      case "BR1":
+        var myimage = d3.select(svg).append('image')
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_011.png')
+          .attr("x", row*70 + xoffset)
+          .attr("y", col*65 + yoffset)
           .attr('width', 50)
           .attr('height', 50);
          break;
       case "BB0":
+      case "BB1":
         var myimage = d3.select(svg).append('image')
-          .attr('xlink:href', 'https://www.pinclipart.com/picdir/big/100-1006999_clipart-silhouette-chess-piece-remix-bishop-alfil-chess.png')
-          .attr("x", row*60 + xoffset*1)
-          .attr("y", col*60 + yoffset*1)
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_009.png')
+          .attr("x", row*75 + xoffset)
+          .attr("y", col*75 + yoffset)
+          .attr('width', 50)
+          .attr('height', 50);
+         break;
+      case "WB0":
+      case "WB1":
+        var myimage = d3.select(svg).append('image')
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_003.png')
+          .attr("x", row*60 + xoffset)
+          .attr("y", col*60 + yoffset)
+          .attr('width', 50)
+          .attr('height', 50);
+         break;
+      case "WQ0":
+      case "WQ1":
+        var myimage = d3.select(svg).append('image')
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_002.png')
+          .attr("x", row*60 + xoffset)
+          .attr("y", col*60 + yoffset)
           .attr('width', 50)
           .attr('height', 50);
          break;
       case "BQ0":
+      case "BQ1":
         var myimage = d3.select(svg).append('image')
-          .attr('xlink:href', 'https://www.clipartmax.com/png/full/204-2042941_clipart-queen-chess-piece-vector.png')
-          .attr("x", row*60 + xoffset*1)
-          .attr("y", col*60 + yoffset*1)
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_002.png')
+          .attr("x", row*60 + xoffset)
+          .attr("y", col*60 + yoffset)
+          .attr('width', 50)
+          .attr('height', 50);
+         break;
+      case "WK0":
+      case "WK1":
+        var myimage = d3.select(svg).append('image')
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_001.png')
+          .attr("x", row*60 + xoffset)
+          .attr("y", col*65 + yoffset)
           .attr('width', 50)
           .attr('height', 50);
          break;
       case "BK0":
+      case "BK1":
         var myimage = d3.select(svg).append('image')
-          .attr('xlink:href', 'http://clipart-library.com/img/1985567.png')
-          .attr("x", row*70 + xoffset*1)
-          .attr("y", col*60 + yoffset*1)
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_007.png')
+          .attr("x", row*60 + xoffset)
+          .attr("y", col*65 + yoffset)
           .attr('width', 50)
           .attr('height', 50);
-         break; 
+         break;
+      case "WP0":
+      case "WP1":
+        var myimage = d3.select(svg).append('image')
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_006.png')
+          .attr("x", row*70 + xoffset)
+          .attr("y", col*60 + yoffset)
+          .attr('width', 50)
+          .attr('height', 50);
+         break;
+      case "BP0":
+      case "BP1":
+        var myimage = d3.select(svg).append('image')
+          .attr('xlink:href', 'https://srv2.imgonline.com.ua/result_img/imgonline-com-ua-cut-image-MQ3ZfwleMfk/image_part_012.png')
+          .attr("x", row*70 + xoffset)
+          .attr("y", col*60 + yoffset)
+          .attr('width', 50)
+          .attr('height', 50);
+         break;
       default:
           d3.select(svg)
             .append("text")
@@ -73,11 +125,17 @@ function printValue(row, col, xoffset, yoffset, value) {
 }
 
 function convert(square) {
-  const squareContent = square.pc.toString()
-  if(squareContent == '') {
-    return '_'
+  const precontent = square.join(pc)
+  let content = ''
+  if ((Black0.pieces).join(precontent).empty()) {
+    content = 'W' + precontent.toString()
   } else {
-    return squareContent
+    content = 'B' + precontent.toString()
+  }
+  if (precontent.empty()) {
+    return '-'
+  } else {
+    return content.toString()
   }
 }
 
@@ -85,32 +143,25 @@ function toCoord(rc) {
   return rc.charAt(3);
 }
 
-// printValue(row1, colA, 5, 25, Board0.places[row1][colA])
-
-function printBoard(startRow, startCol, board) {
+function printBoard(startRow, startCol) {
   let row = startRow;
   let col = startCol;
-
   
-  for (r = 0; r < 5; r++) {
-    printValue(0, r, 50, 50, toCoord(row.toString()));
-    for (c = 1; c < 6; c++) {
-      const square = Board0.places[row][col];
-      printValue(c, r, 50, 50, convert(square));
-//       printValue(c, r, 50, 50, square.coord.toString())
-//       console.log(convert(square));
+  for (r = 0; r < BOARD_SIZE; r++) {
+    printValue(0, r, OFFSET, OFFSET, toCoord(row.toString()));
+    for (c = 1; c < BOARD_SIZE + 1; c++) {
+      const square = coord.join(col).join(row)
+      printValue(c, r, OFFSET, OFFSET, convert(square));
       col = col.c_next;
     }
     col = startCol;
     row = row.r_prev;
   }
   
-  for (c = 1; c < 6; c++){
-    printValue(c, 8, 50, 50, toCoord(col.toString()));
+  for (c = 1; c < BOARD_SIZE + 1; c++) {
+    printValue(c, BOARD_SIZE, OFFSET, OFFSET, toCoord(col.toString()));
     col = col.c_next;
   }
   
 }
-
-printBoard(row4, colA, Board0);
-
+printBoard(row5, colA)
